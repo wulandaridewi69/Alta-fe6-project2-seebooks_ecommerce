1f831a4250f3ac432c48e92a8f58e2d36b877883
rizqiirfand <rizqiirfan23@gmail.com>
2022-06-29 13:46:37 +0700

    update login,header,homepage

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,36 +1,46 @@
-import React, { useEffect,useState } from 'react';
+import React, { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { TokenContext } from '../utils/context';
 import Cart from '../assets/cart.png';
 import '../styles/App.css'
 import Search from '../assets/search.png'
-import { useNavigate } from 'react-router-dom';
 import Button from './button';
 
 const Header = () => {
     const navigate = useNavigate()
-    const [isLogin, setIsLogin] = useState(localStorage.getItem('userJwt'))
-    useEffect(() => {
-        if (!isLogin) {
-            localStorage.removeItem('userJwt')
-        }
-    },[isLogin])
+    const { token,setToken } = useContext(TokenContext);    
+
+    const handleLogout = () => {
+        localStorage.removeItem("token");
+        setToken("0");
+    }
     return (
-        <div className='header'>
+        <div className='header h-auto'>
             <div className='container'>
-                <div className='flex items-center py-3 gap-4'>
-                    {isLogin ? (
+                <div className='flex items-center py-2 gap-4'>
+                    {token !== '0' ? (
                         <>
                             <div className='col-6 '>
                                 <h1 className='logo font-bold text-4xl cursor-pointer' onClick={()=>navigate('/')}>SEEBOOKS</h1>
                             </div>
                             <div className='col-4 flex'>
-                                <img src={Search} className='icon-search bg-white p-2 ' alt=''/>
-                                <input className='search w-full'></input>
+                                <img src={Search} className='h-full rounded-l-md bg-white p-2' alt=''/>
+                                <input className='w-full text-black p-1 rounded-r-md'></input>
                             </div>
-                            <div className='col-1'>
-                                <img src={Cart} className='cart' alt=''/>
+                            <div className='col-1 flex justify-center'>
+                                <img src={Cart} className='cursor-pointer' alt=''/>
                             </div>
-                            <div className='col-1 cursor-pointer'>
-                                <div className='h-10 w-10 rounded-full bg-white' onClick={()=>setIsLogin(false)}></div>
+                            <div className='col-1 group inline-block relative cursor-pointer text-center'>
+                                <button className="h-12 w-12 bg-white rounded-full hover:bg-teal-900 cursor-pointer text-white">
+                                </button>
+                                <ul className="absolute hidden text-gray-700 pt-1 group-hover:block w-full">
+                                    <li className="text-white">
+                                        <div className="bg-teal-600 hover:bg-teal-900 py-2 px-4 block whitespace-no-wrap cursor-pointer">Profile</div>
+                                    </li>
+                                    <li className="text-white">
+                                        <div className="bg-teal-600 hover:bg-teal-900 py-2 px-4 block whitespace-no-wrap cursor-pointer" onClick={()=>handleLogout()}>LogOut</div>
+                                    </li>
+                                </ul>
                             </div>
                         </>
                     ) : (
@@ -39,8 +49,8 @@ const Header = () => {
                                 <h1 className='logo font-bold text-4xl cursor-pointer' onClick={()=>navigate('/')}>SEEBOOKS</h1>
                             </div>
                             <div className='col-4 flex'>
-                                <img src={Search} className='icon-search bg-white p-2 ' alt=''/>
-                                <input className='search w-full'></input>
+                                <img src={Search} className='h-full rounded-l-md bg-white p-2' alt=''/>
+                                <input className='w-full text-black p-1 rounded-r-md'></input>
                             </div>
                             <div className='col-3 flex gap-4 justify-end'>
                                 <Button className="border-[0.1rem] border-white rounded-md font-bold px-8 py-1 hover:bg-white hover:text-teal-600" onClick={()=>navigate('/login')}>Login</Button>
