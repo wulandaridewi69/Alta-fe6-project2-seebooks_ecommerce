92087b5c145f003c7d7a9fc9c740bade1e2c4d3c
wulandaridewi69 <dewi.wulandari.ruslan@gmail.com>
2022-06-30 11:33:37 +0700

    histories

--- src/pages/History.jsx
+++ src/pages/History.jsx
@@ -8,7 +8,7 @@ import { useState } from 'react'
 import { useEffect } from 'react'
 import { useRef } from 'react'
 
-const Histories = (props) => {
+const CreateProduct = (props) => {
 
     const [edit, setEdit] = useState({
         id: '',
@@ -71,8 +71,8 @@ const Histories = (props) => {
 
     return (
         <Layout>
-            <div className="container">
-                <div className="row">
+            <div className='container'>
+                <div className='row'>
                     <div className='col-2 bg-slate-200'>
                         <div className='product flex gap-x-5 mt-24'>
                             <img src={Product} />
@@ -84,10 +84,76 @@ const Histories = (props) => {
                             <p>History</p>
                         </div>
                     </div>
+                    <div className='col-5'>
+                        <div className='p-3 mb-3 text-2xl font-bold'>
+                            Update Product
+                        </div>
+                        <br/>
+                        <div className='history flex gap-x-5'>
+                            <img src={History} />
+                            <p>History</p>
+                        </div>
+                        <form>
+                            <fieldset className='px-2 border-slate-300'>
+                                <legend className='legend'>Book Title</legend>
+                                <input ref={inputRef} name='text' onChange={(e) => handleChange(e, "username")} value={username} className='form input pb-2' placeholder='Physics' />
+                            </fieldset>
+                            <br />
+                            <fieldset className='px-2 border-slate-300'>
+                                <legend className='legend'>Writer</legend>
+                                <input ref={inputRef} name='text' onChange={(e) => handleChange(e, "username")} value={username} className='form input pb-2' placeholder='Kenneth Krane' />
+                            </fieldset>
+                            <br />
+                            <fieldset className='px-2 border-slate-300'>
+                                <legend className='legend'>Description</legend>
+                                <textarea className='input w-full' ref={inputRef} name='text' onChange={(e) => handleChange(e, "address")} value={address} placeholder='The wonders of the speed of light, of gravitational waves, of molecular motors explained. Be amazed by the motion of stones, light, stars, atoms, muscles and empty space. Interesting facts. Fascinating puzzles. Beautiful Images. Clear explanations.' />
+                            </fieldset>
+                            <br />
+                            <fieldset className='px-2 border-slate-300'>
+                                <legend className='legend'>Pages</legend>
+                                <input ref={inputRef} name='text' onChange={(e) => handleChange(e, "username")} value={username} className='form input pb-2' placeholder='1024' />
+                            </fieldset>
+                            <br />
+                            <fieldset className='px-2 border-slate-300'>
+                                <legend className='legend'>ISBN</legend>
+                                <input ref={inputRef} name='text' onChange={(e) => handleChange(e, "username")} value={username} className='form input pb-2' placeholder='9386105268' />
+                            </fieldset>
+                        </form>
+                    </div>
+                    <div className='col-5'>
+                        <form className='pt-20'>
+                            <fieldset className='px-2 border-slate-300'>
+                                <legend className='legend'>Publisher</legend>
+                                <input ref={inputRef} name='text' onChange={(e) => handleChange(e, "username")} value={username} className='form input pb-2' placeholder='Griffith' />
+                            </fieldset>
+                            <br />
+                            <fieldset className='px-2 border-slate-300'>
+                                <legend className='legend'>Price</legend>
+                                <input ref={inputRef} name='text' onChange={(e) => handleChange(e, "username")} value={username} className='form input pb-2' placeholder='$ 21.58' />
+                            </fieldset>
+                            <br />
+                            <fieldset className='px-2 border-slate-300'>
+                                <legend className='legend'>Stock</legend>
+                                <input ref={inputRef} name='text' onChange={(e) => handleChange(e, "username")} value={username} className='form input pb-2' placeholder='10' />
+                            </fieldset>
+                            <br />
+                            <fieldset className='px-2 border-slate-300'>
+                                <legend className='legend'>Upload Image</legend>
+                                <label for='myfile'>Choose File</label>
+                                <input ref={inputRef} name='text' onChange={(e) => handleChange(e, "username")} input='file' id='myFile' multiple value={username} className='form input pb-2' placeholder='Upload' />
+                            </fieldset>
+                        </form>
+                        <br/>
+                        <form>
+                        <Button className='btn-primary text-white text-sm inline-block font-bold border-0 px-5 py-2 decoration-0 bg-teal-500 hover:bg-teal-600 ... text-center' text='Save' onClick={handleSubmit}>Submit</Button>
+                        </form>
+                    </div>
                 </div>
             </div>
         </Layout>
     )
+
+
 }
 
-export default Histories;
+export default CreateProduct;
